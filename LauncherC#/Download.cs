32a49954415125beef1a259bd044d286633a23fd
ForKill <gtateam@mail.ru>
2023-09-28 17:04:43 +0400

    Добавьте файлы проекта.

---
+++ LauncherC#/Download.cs
@@ -0,0 +1,14 @@
+﻿namespace LauncherC_
+{
+  internal class Download
+  {
+    private string Url;
+    private string FileName;
+
+    public Download(string url, string fileName)
+    {
+      Url = url;
+      FileName = fileName;
+    }
+  }
+}
